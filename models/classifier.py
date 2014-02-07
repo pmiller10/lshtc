80e92f5c626a66165a183c68ec3176351034a22f
pmiller10 <pmiller987@gmail.com>
2014-02-07 03:24:53 +0000

    Classifier class

---
+++ models/classifier.py
@@ -0,0 +1,17 @@
+from sklearn.linear_model import LogisticRegression
+
+class Classifier:
+
+    def __init__(self, model):
+        self.model = model
+
+    def train(self, data, targets):
+        self.model.fit(data, targets)
+    
+    def predict(self, data):
+        return self.model.predict(data)
+
+if __name__ == "__main__":
+    logistic = Classifier(LogisticRegression())
+    logistic.train([[0.],[1.]], [0,1])
+    print logistic.predict([1.])
